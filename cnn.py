9c02192a6ec6f4d580b9b9a2f46e89775775782e
Aditya Sharma <adisharma1076@gmail.com>
2020-09-13 10:19:25 +0530

    Convolution and Highway Model

--- cnn.py
+++ cnn.py
@@ -1,12 +1,33 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+import torch
+import torch.nn as nn
 
-"""
-CS224N 2018-19: Homework 5
-"""
+class CNN(nn.Module):
+    
+    def __init__(self,embed_size,feature_size,kernel_size,max_word_length):
+        super(CNN,self).__init__()
+        self.conv = nn.Conv1d(in_channels=embed_size,out_channels=feature_size,kernel_size=kernel_size,bias=True)
+        self.Relu = nn.ReLU()
+        self.max_pool = nn.MaxPool1d(max_word_length-kernel_size+1)
 
-### YOUR CODE HERE for part 1i
+    def forward(self,x_reshaped):
+        conv_out = self.conv(x_reshaped)
+        Relu_out = self.Relu(conv_out)
+        pool_out = self.max_pool(Relu_out)
 
+        return pool_out
 
-### END YOUR CODE
+if __name__ =='__main__':
+    embed_size = 16
+    kernel_size = 5
+    max_word_length = 21
+    batch_size = 2 
+    features = 16
+    model = CNN(embed_size,features,kernel_size,max_word_length)
+
+    t3 = torch.randn(batch_size, embed_size,max_word_length)
+
+    output = model(t3)
+    print(output.shape)
+    output = output.squeeze(2)
+    print(output)
 
--- highway.py
+++ highway.py
@@ -1,12 +1,35 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-"""
-CS224N 2018-19: Homework 5
-"""
+import torch
+import torch.nn as nn
 
-### YOUR CODE HERE for part 1h
+class Highway(nn.Module):
+    
+    def __init__(self,embed_size):
+        super(Highway,self).__init__()
+        self.projection = nn.Linear(embed_size,embed_size,bias=True)
+        self.gate = nn.Linear(embed_size,embed_size,bias=True)
+        self.Relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        self.dropout = nn.Dropout(0.3)
+    def forward(self,x_conv_out):
+        proj_out = self.projection(x_conv_out)
+        proj_relu = self.Relu(proj_out)
 
+        gate_out = self.gate(proj_relu)
+        gate_sigmoid = self.sigmoid(gate_out)
 
-### END YOUR CODE 
+        highway_out = torch.mul(proj_relu,gate_sigmoid) + torch.mul(x_conv_out,(1-gate_sigmoid))
+        x_word_embed = self.dropout(highway_out)
+        
+        return x_word_embed
 
+#     ###### Test
+# if __name__ == "__main__":
+#     batch_size = 2
+#     embed_size = 6
+#     model = Highway(embed_size)
+#     criterion = nn.CrossEntropyLoss()
+#     t3 = torch.randn(batch_size, embed_size)
+    
+#     output = model(t3)
+#     print(output)
